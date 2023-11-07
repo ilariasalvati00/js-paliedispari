98828a425834773dfd9d028a2135c9c2bb3ffcbd
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-11-07 22:44:31 +0100

    palindroma fatta

---
+++ Palindroma/main.js
@@ -0,0 +1,23 @@
+const parola = prompt ("inserisci parola"); 
+palindroma(parola);
+
+
+
+
+function palindroma(stringa){
+    let stringa2 = stringa.split("").reverse().join("");
+    if (stringa==stringa2){
+        console.log(`la stringa ${stringa} è palindroma`);
+    } else{
+        console.log(`la stringa ${stringa} non è palindroma`);
+    }
+}
+
+
+
+    if (palindroma == true){
+        console.log(`la stringa ${stringa} è palindroma`);
+    } else{
+        console.log(`la stringa ${stringa} non è palindroma`);
+    }
+}
